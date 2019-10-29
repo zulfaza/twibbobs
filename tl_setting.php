0b17c46888d393ec3a0350ab92282352c7f961bc
zulfaza <32305083+zulfaza@users.noreply.github.com>
2019-10-29 21:03:52 +0700

    fix

--- tl_setting.php
+++ tl_setting.php
@@ -61,9 +61,9 @@ if (isset($_POST['edit'])) {
   
       
       $id=$_POST['id'];
-      $name=$_POST['name'];
-      $username=$_POST['username'];
-      $email=$_POST['email'];
+      $name=filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+      $username=filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+      $email=filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
       
       $sql = "UPDATE users SET username='$username', email='$email', name='$name', photo='$pp' WHERE id=$id";
       mysqli_query($koneksi, $sql) or die(header("Location: tl_setting.php?pesan=setting_fail&edit=$id"));
