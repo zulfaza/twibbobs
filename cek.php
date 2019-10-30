d6afeb9c2f3602ae7780ae6b024aa32c7c93bf7a
zulfaza <32305083+zulfaza@users.noreply.github.com>
2019-10-30 19:14:01 +0700

    Update cek.php

--- cek.php
+++ cek.php
@@ -57,6 +57,7 @@
 
     <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
       <div class="card-body">
+      <div class="row>
 <table border="1">
     <tr><th>NO</th><th>id</th><th>username</th><th>email</th><th>nama</th><th>Level</th><th>Action</th></tr>
     <?php
@@ -76,6 +77,7 @@
     }
     ?>
 </table>
+          </div>
       </div>
     </div>
   </div>
