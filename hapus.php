1a2749455fb5422d14b1e8f99c5ba79b5eb3b300
zulfaza <32305083+zulfaza@users.noreply.github.com>
2019-10-30 16:31:21 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -1,7 +1,15 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","twibbon");
+require_once("config.php"); 
 $id = $_GET['id'];
-mysqli_query($koneksi,"DELETE FROM tb_images WHERE id='$id'") or die(mysqli_error($koneksi));
 
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    mysqli_query($koneksi,"DELETE FROM tb_images WHERE id='$id'") or die(mysqli_error($koneksi));
 header("location:./timeline.php?pesan=hapus");
+}
+if(isset($_GET['akun'])){
+    $id = $_GET['akun'];
+    mysqli_query($koneksi,"DELETE FROM users WHERE id='$id'") or die(mysqli_error($koneksi));
+    header("location:./cek.php?pesan=hapus");
+    }
 ?>
